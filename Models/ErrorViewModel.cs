2f74184509ee0e523e57f4d32f0a70281c50aed0
Jorge_Bustos <jorge_bustos@lasec.com.mx>
2024-08-26 21:15:54 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ResumeJorgeBustos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
